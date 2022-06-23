db577d5a3da092c957d8bb3b1bfcfb4d769fab84
pliduino <liduino@usp.br>
2022-06-23 20:21:27 -0300

    Added Helicopter Meshes

--- Main.py
+++ Main.py
@@ -10,23 +10,37 @@ windowManager.show()
 shader = Shader.Shader(3)
 
 
-base = Objects.MeshObject()
-base.load_mesh_file("Meshes/SM_Terreno.obj")
-base.set_texture(shader.load_texture("Textures/T_Pedra.jpg"))
-base.set_scale(20, 20, 20)
+heli_base = Objects.MeshObject()
+heli_base.load_mesh_file("Meshes/low-poly-helicopter_base.obj")
+heli_base.set_texture(shader.load_texture("Textures/T_Pedra.jpg"))
 
-shader.bind_mesh(base)
+heli_helice = Objects.MeshObject()
+heli_helice.load_mesh_file("Meshes/low-poly-helicopter_helice.obj")
+heli_helice.set_texture(shader.load_texture("Textures/T_Pedra.jpg"))
+heli_helice.set_position(0, 1.3, .57)
+
+heli_helice2 = Objects.MeshObject()
+heli_helice2.load_mesh_file("Meshes/low-poly-helicopter_helice2.obj")
+heli_helice2.set_texture(shader.load_texture("Textures/T_Pedra.jpg"))
+heli_helice2.set_position(.17, 1.4, -9.1)
+
+
+shader.bind_mesh(heli_base)
+shader.bind_mesh(heli_helice)
+shader.bind_mesh(heli_helice2)
+
 shader.upload_binded_meshes()
-
-print(base)
-print("\n")
-print(shader._vertices_list)
 
 while windowManager.loop():
     windowManager.clear()
-    base.rotate(0, .1, 0)
+    
+    heli_helice.rotate(0, 20, 0)
+    heli_helice2.rotate(20, 0, 0)
+
     # Drawing Objects
-    shader.draw_object(base)
+    shader.draw_object(heli_base)
+    shader.draw_object(heli_helice)
+    shader.draw_object(heli_helice2)
     
 
     windowManager.update(shader.get_program())
